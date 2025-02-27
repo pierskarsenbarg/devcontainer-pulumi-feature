e2165ca038eacaec0949f7babb6b5ec964d73170
Piers Karsenbarg <piers@pulumi.com>
2025-02-27 18:13:41 +0000

    Added pulumi installer

---
+++ src/pulumi/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'pulumi'"
+
+echo "The provided Pulumi Version is: $PULUMIVERSION"
+
+PULUMI_HOME=$_REMOTE_USER_HOME
+
+echo "The effective dev container remoteUser is '$_REMOTE_USER'"
+echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
+
+echo "The effective dev container containerUser is '$_CONTAINER_USER'"
+echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
+
+echo "PULUMI_HOME is '$PULUMI_HOME'"
+
+echo "Installing Pulumi..."
+
+if [[ -z "${PULUMIVERSION}"]]; then
+    curl -fsSL https://get.pulumi.com | sh -s -- --install-root $_REMOTE_USER_HOME
+
+else
+    curl -fsSL https://get.pulumi.com | sh -s -- --install-root $_REMOTE_USER_HOME --version $PULUMIVERSION 
+fi
+
+echo "Pulumi Installed..."
